dfe06d8ffb9e62d57dfe6d16d6b27de3f8f9a94a
Leo Lei <leolei@berkeley.edu>
2018-04-07 17:26:18 +0800

    handle required parameters when missing

--- src/controllers/emailController.js
+++ src/controllers/emailController.js
@@ -2,18 +2,19 @@ import ReliableMailer from '../services/ReliableMailer'
 
 const create = async (req, res) => {
   const mailer = new ReliableMailer()
+
+  if (!req.body.subject) { res.status(400).json({error: 'Subject is Required.'}); return }
+  if (!req.body.to_address) { res.status(400).json({error: 'Recipient Email is Required.'}); return }
+  if (!req.body.message) { res.status(400).json({error: 'Message is Required.'}); return }
+
   const {succeeded, error, tryCount} = await mailer.send({
     from: 'Aftership Challenger <aftership.email.challenge@gmail.com>',
-    subject: req.body.subject || 'No Subject',
-    to: req.body.to_address || '4leolei@gmail.com',
-    text: req.body.message || 'testing mailgun',
+    subject: req.body.subject,
+    to: req.body.to_address,
+    text: req.body.message,
   })
 
-  if (succeeded) {
-    res.json({tryCount})
-  } else {
-    res.status(500).json({error, tryCount})
-  }
+  res.status(succeeded ? 200 : 500).json({error, tryCount})
 }
 
 export default {create}
